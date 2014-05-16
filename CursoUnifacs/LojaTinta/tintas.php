68d38b9bfa8a0ad06f459517a93c4656baa7c7d8
Jorge <Jorge@Zeus>
2014-05-15 22:57:36 -0300

    - Alteração da Variavel $latas - tirando 18 para 10

--- CursoUnifacs/LojaTinta/tintas.php
+++ CursoUnifacs/LojaTinta/tintas.php
@@ -2,7 +2,7 @@
 	function fatura($request){
 		$area = $request['area'];
 		$litros = ceil($area/3);
-		$latas = ceil($litros/18);
+		$latas = ceil($litros/10);
 		$custo = number_format(($latas * 80.0), 2, ',', '.');
 		printf('Quantidade de latas: %s Custo Total: R$ %s', $latas, $custo);
 	}
